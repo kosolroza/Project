8ec3617245441c1d720803c5358825f61f20cad6
m3ngTheBoss <hortkimmeng168@gmail.com>
2025-05-10 18:56:04 +0900

    newly updatee

--- Employee.h
+++ Employee.h
@@ -1,2 +1,16 @@
 #include<iostream>
+#include<fstream>
 using namespace std;
+
+void displayEmployees() {
+    fstream file;
+    string line;
+    file.open("Employees_list.csv", ios::in);
+    if(!file){
+        cout << "Error couldn't open file" << endl ;
+    }
+    while(getline(file,line)){
+        cout << line << endl;
+    }
+    file.close();
+}
